ab9451deeba86bf3d6c8305cb710ece99c0fd4bf
harshs14 <harsh.singh1411@gmail.com>
2018-08-29 01:35:07 +0530

    collision detection progress

--- gamingpage/1.js
+++ gamingpage/1.js
@@ -2,21 +2,35 @@
 var hero = new Object();
 var hero_fire = new Object();
 var enemy1 = new Object();
+var enemy2 = new Object();
+var enemy3 = new Object();
+var count = 0;
+var score = 0;
 hero.element = 'hero';
-hero.x = 650;
-hero.y = 460;
+hero.x = 400;
+hero.y = 600;
 hero.width = 150;
 hero.height = 150;
 hero_fire.element = 'hero_fire';
-hero_fire.x = 645;
-hero_fire.y = 430;
+hero_fire.x = 411;
+hero_fire.y = 550;
 hero_fire.width = 20;
 hero_fire.height = 20;
 enemy1.element = 'enemy1';
-enemy1.x = 650;
+enemy1.x = 400;
 enemy1.y = 20;
 enemy1.width = 150;
 enemy1.height = 150;
+enemy2.element = 'enemy2';
+enemy2.x = 200;
+enemy2.y = 20;
+enemy2.width = 150;
+enemy2.height = 150;
+enemy3.element = 'enemy3';
+enemy3.x = 600;
+enemy3.y = 20;
+enemy3.width = 150;
+enemy3.height = 150;
 
 function initials() {
     hero.margin_left = 0;
@@ -28,6 +42,7 @@ function initials() {
     hero_fire.margin_top = 0;
     hero_fire.margin_bottom = 130;
 }
+
 function setPosition(obj) {
     var e = document.getElementById(obj.element);
     e.style.left = obj.x + 'px';
@@ -84,7 +99,7 @@ function keypressed(keyCode, value) { //experiment
 }
 // handles the controls
 function handleControls() {
-    if (control.up == true)//experiment
+    if (control.up == true) //experiment
     {
         hero.y -= 4;
         hero_fire.y -= 4;
@@ -134,6 +149,8 @@ function showChanges() {
     setPosition(hero);
     setPosition(hero_fire);
     setPosition(enemy1);
+    setPosition(enemy2);
+    setPosition(enemy3);
 }
 // looping function calls itself in every 2 mili sec
 function loop() {
@@ -141,7 +158,9 @@ function loop() {
         initials();
         handleControls();
         showChanges();
-        collision();
+        collisionenemy1();
+        collisionenemy2();
+        collisionenemy3();
         lastUpdate = new Date().getTime();
     }
     setTimeout('loop();', 2);
@@ -170,12 +189,64 @@ function shooting(pos) {
     pos.y -= 5;
     setTimeout('shooting(hero_fire)', 20);
 }
-function collision() {
+
+function enemy1dead() {
+    document.getElementById("enemy1").setAttribute("src", " ");
+    // if (count==3)
+    //    {
+    //      setTimeout(nextenemywave,1000);
+    //    }
+}
+
+function enemy2dead() {
+    document.getElementById("enemy2").setAttribute("src", " ");
+    // if (count == 3)
+    // {
+    //     setTimeout(nextenemywave, 1000);
+    // }
+}
+
+function enemy3dead() {
+    document.getElementById("enemy3").setAttribute("src", " ");
+    // if (count == 3) 
+    // {
+    //     setTimeout(nextenemywave, 1000);
+    // }
+}
+
+function collisionenemy1() {
     if ((hero_fire.x < (enemy1.x + enemy1.width)) && ((hero_fire.x + hero_fire.width) > enemy1.x) &&
         (hero_fire.y < (enemy1.y + enemy1.height))) {
-        console.log("hit");
-
+        console.log("enemy1 hit");
+        count++;
+        document.getElementById("enemy1").setAttribute("src", "blast.png");
+        setTimeout(enemy1dead, 1000);
     }
 }
 
+function collisionenemy2() {
+    if ((hero_fire.x < (enemy2.x + enemy2.width)) && ((hero_fire.x + hero_fire.width) > enemy2.x) &&
+        (hero_fire.y < (enemy2.y + enemy2.height))) {
+        console.log("enemy hit 2");
+        count++;
+        document.getElementById("enemy2").setAttribute("src", "blast.png");
+        setTimeout(enemy2dead, 1000);
+    }
+}
+
+function collisionenemy3() {
+    if ((hero_fire.x < (enemy3.x + enemy3.width)) && ((hero_fire.x + hero_fire.width) > enemy3.x) &&
+        (hero_fire.y < (enemy3.y + enemy3.height))) {
+        console.log("enemy3 hit");
+        count++;
+        document.getElementById("enemy3").setAttribute("src", "blast.png");
+        setTimeout(enemy3dead, 1000);
+    }
+}
+// function nextenemywave()
+// {
+//     document.getElementById("enemy1").setAttribute("src", "enemy2.png");
+//     document.getElementById("enemy2").setAttribute("src", "enemy2.png");
+//     document.getElementById("enemy3").setAttribute("src", "enemy3.png");
+// }
 loop();
